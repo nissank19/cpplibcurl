63af0a3f715c77e38cbc0293941471d7e28b3389
nissank19 <fabiannissankastudy@gmail.com>
2026-02-26 14:29:48 +0100

    created twrite call which gets the packets, pck size numb of packs and the content this method appends the values to the end of readbuffer

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/c++libcurl")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/cooking")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <string>
 #include <curl/curl.h>
 
+//ima take da
+size_t WriteCallback(void* contents, size_t size, size_t numbe, std::string* userp) {
+    userp->append((char*)contents, size * numbe);
+    return size * numbe;
+}
+
 int main() {
-    CURL *curl = curl_easy_init();
-    if(curl) {
-        std::cout << "Success! The 'Mapping' to Curl is complete." << std::endl;
-        curl_easy_cleanup(curl);
-    } else {
-        std::cout << "Failed to initialize. Check the library path." << std::endl;
-    }
+    CURL* curl = curl_easy_init(); //pointer to startup up the curl
+    std::string readBuffer;
+
+
     return 0;
 }
