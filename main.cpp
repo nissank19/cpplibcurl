63ea4217d2f8ecdd8f0c38529d5aef3ec8a2e565
nissank19 <fabiannissankastudy@gmail.com>
2026-02-27 15:02:47 +0100

    checked if curl is a null pointer, connected to binance api, host/peer not checked.wrote function and data

--- main.cpp
+++ main.cpp
@@ -7,10 +7,31 @@ size_t WriteCallback(void* contents, size_t size, size_t numbe, std::string* use
     userp->append((char*)contents, size * numbe);
     return size * numbe;
 }
+//https://api.binance.com
 
 int main() {
     CURL* curl = curl_easy_init(); //pointer to startup up the curl
     std::string readBuffer;
+    CURLcode res;
+    if (curl==nullptr) {
+        std::cout << "curl_easy_init failed" << std::endl;
+        return -1;
+    }
+    curl_easy_setopt(curl,CURLOPT_URL,"https://api-gcp.binance.com/api/v3/aggTrades?symbol=BNBETH");
+   // curl_easy_setopt(curl,CURLOPT_FOLLOWLOCATION,1L);
+    //curl_easy_setopt(curl,CURLOPT_USERAGENT,"yeso");
+    curl_easy_setopt(curl,CURLOPT_SSL_VERIFYHOST,0L);
+    curl_easy_setopt(curl,CURLOPT_SSL_VERIFYPEER,0L);
+    curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res=curl_easy_perform( curl);
+    if (res!=CURLE_OK) {
+        std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        return -1;
+    }
+    curl_easy_cleanup(curl);
+    std::cout << readBuffer << std::endl;
+
 
 
     return 0;
